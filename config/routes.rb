27a2b917befe11a8e0ee2c0d2aba9b92432a1c74
Aleksey Voronkov <voronkov@gmail.com>
2016-12-02 14:20:49 +0200

    Add comments to post

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
-  
+
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
-  resources :articles
+  resources :articles do 
+  	resources :comments
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
