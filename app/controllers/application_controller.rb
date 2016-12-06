c0e088d3181a19feca0904f410dcf4e6ed98d4a7
Aleksey Voronkov <voronkov@gmail.com>
2016-12-06 14:58:53 +0200

    Show username in header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-	private
+  protected
 
-	def configure_permitted_parameters
-	   devise_parameter_sanitizer.for(:sign_up) << :username
-	   devise_parameter_sanitizer.for(:sign_in) << :username
-	   devise_parameter_sanitizer.for(:account_update) << :username 
-	end 
-
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 
 end
