8bedc239cbbea24f25ec2bdcfaa1d840bbd3fc9e
Aleksey Voronkov <voronkov@gmail.com>
2016-12-08 15:13:55 +0200

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -15,4 +15,13 @@ end
 			expect(article.subject).to eq 'Lorem Ipsum'
 		end
 	end
+
+	describe "#last_comment" do 
+		it "returns the last comment" do 
+			article = create(:article_with_comments)
+
+			expect(article.last_comment.body).to eq "comment body 3"
+		end
+	end
+
 end
